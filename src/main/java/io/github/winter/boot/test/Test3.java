5ab0b315e5da282a7ce302ccf7c8399e97a5fbc5
changebooks <changebooks@qq.com>
2025-06-01 13:09:17 +0800

    Create Test3.java

---
+++ src/main/java/io/github/winter/boot/test/Test3.java
@@ -0,0 +1,18 @@
+package io.github.winter.boot.test;
+
+/**
+ * 测试3
+ *
+ * @author changebooks@qq.com
+ */
+public final class Test3 {
+    /**
+     * 运行测试
+     *
+     * @param args 参数列表
+     */
+    public static void main(String[] args) {
+        System.out.println("Test3 main");
+    }
+
+}
