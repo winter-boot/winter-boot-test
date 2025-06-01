45fc0924706a5af5a6ebcc63b40ca106ca29d80d
changebooks <changebooks@qq.com>
2025-06-01 17:13:45 +0800

    3

---
+++ src/main/java/io/github/winter/boot/test/Test4.java
@@ -0,0 +1,10 @@
+package io.github.winter.boot.test;
+public final class Test4 {
+    /**
+     * 运行测试
+     */
+    public static void main(String[] args) {
+        System.out.println("Test4 main");
+    }
+
+}
---
+++ src/main/java/io/github/winter/boot/test/Test5.java
@@ -0,0 +1,20 @@
+package io.github.winter.boot.test;
+
+public final class Test5 {
+
+    public <T> T t1(T t) {
+        return t;
+    }
+
+    public Integer t2(Integer a, Integer b) {
+        return a + b;
+    }
+
+    /**
+     * t3
+     */
+    public Integer t3(Integer a, Integer b) {
+        return a - b;
+    }
+
+}
